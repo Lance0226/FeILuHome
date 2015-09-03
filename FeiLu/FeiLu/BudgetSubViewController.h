3a90b432ae003cd7035524eaed9cb68c5623aa35
Lance0226 <lance@lances-Mac-mini.local>
2015-09-03 18:55:18 +0800

    666
    666

--- FeiLu/FeiLu/BudgetSubViewController.h
+++ FeiLu/FeiLu/BudgetSubViewController.h
@@ -12,11 +12,36 @@
 @property (nonatomic,retain) NSNumber  *xmlIndex;
 @property (nonatomic,retain) NSNumber  *xmlSubIndex;
 
-typedef struct{
+typedef struct
+{
     int type;        //0为1级节点，1为2级节点
+    int category;    //1级节点均为0，2级节点第一类为0，第二类为1，第三类为2
+    int parent;      //父节点序号，从零开始，没有父节点为－1
     int index;
-    
-    
-}BudgetTypeList;
+}BudgetLevelGroup;
 
+
+typedef struct      //1级节点数据结构
+{
+    const char*  name;
+    const char*  budget;
+}BudgetLevelOneNodeGroup;
+
+
+
+typedef struct    //2级节点第一类数据结构
+{
+    const char* name;
+}BudgetLevelTwoTypeOneGroup;
+
+
+typedef struct    //2级节点第二类类数据结构
+{
+    const char* name;
+}BudgetLevelTwoTypeTwoGroup;
+
+typedef struct   //2级节点第三类类数据结构
+{
+    const char* name;
+}BudgetLevelTwoTypeThreeGroup;
 @end
--- FeiLu/FeiLu/BudgetSubViewController.m
+++ FeiLu/FeiLu/BudgetSubViewController.m
@@ -12,9 +12,10 @@
 @interface BudgetSubViewController ()
 
 @property (retain,nonatomic) UITableView     *subBudgetTableView;
-@property (retain,nonatomic) NSMutableArray  *arrNode;            //一级二级分级队列
-@property (retain,nonatomic) NSMutableArray  *arrNode1Name; 
-@property (retain,nonatomic) NSMutableArray  *arrNode1Budget;
+@property (retain,nonatomic) NSMutableArray  *arrNodeType;            //一级二级分级队列
+
+@property (retain,nonatomic) NSMutableArray  *arrLevelOneNode;       //一级节点数组
+@property (retain,nonatomic) NSMutableArray  *arrLevelTwoNode;
 
 
 @end
@@ -42,9 +43,9 @@
                                        self.view.frame.size.width,
                                        self.view.frame.size.height);
     
-    self.arrNode=[[NSMutableArray alloc]init];
-    self.arrNode1Name=[[NSMutableArray alloc]init];
-    self.arrNode1Budget=[[NSMutableArray alloc]init];
+    self.arrNodeType=[[NSMutableArray alloc]init];
+    self.arrLevelOneNode=[[NSMutableArray alloc]init];
+    self.arrLevelTwoNode=[[NSMutableArray alloc]init];
     
     self.subBudgetTableView.frame = tableViewFrame;
     [self.view addSubview:_subBudgetTableView];
@@ -71,20 +72,67 @@
         
 
         NSArray *arrNode3=[node2 children];
-        int i=0;
-        for (GDataXMLElement *node3 in arrNode3)
+        int m=-1;
+        int n=-1;
+        for (GDataXMLElement *node3 in arrNode3) //一级列表循环
         {
-            i++;
-            NSString *Node3Name=[[node3 attributeForName:@"name"]stringValue];
-            NSString *Node3Budget=[[node3 attributeForName:@"budget"]stringValue];
+            m++;
+            //一级结点数据类型部分
+            NSValue *node3Type=[self setBudgetLevelGroupWithType:0 Category:0 Parent:-1 Index:m];
+            [self.arrNodeType addObject:node3Type];
             
-            BudgetTypeList budgetTypeGroup;
+            //一级结点数据内容部分
+            NSString *node3Name=[[node3 attributeForName:@"name"]stringValue];
+            NSString *node3Budget=[[node3 attributeForName:@"budget"]stringValue];
+            NSValue *node3Content=[self setBudgetLevelOneNodeWithName:node3Name Budget:node3Budget];
+            [self.arrLevelOneNode addObject:node3Content];
             
-            
-            NSValue *budgetType=[NSValue value:<#(const void *)#> withObjCType:<#(const char *)#>]
-            
-            [self.arrNode1Name addObject:Node3Name];
-            [self.arrNode1Budget addObject:Node3Budget];
+            NSArray *arrNode4=[node3 children];
+            for (GDataXMLElement *node4 in arrNode4)
+            {
+                n++;
+                //根据传入xml参数序号的不同，选择不同的结构体封装
+                switch ([self.xmlSubIndex integerValue])
+                {
+                 case 0:{
+                         //第一类二级结点数据类型
+                         NSValue *node4Type=[self setBudgetLevelGroupWithType:1 Category:0 Parent:m Index:n];
+                         [self.arrNodeType addObject:node4Type];
+                     
+                         //第一类二级结点数据内容
+                         NSString *node4Name=[[node4 attributeForName:@"item_name"]stringValue];
+                         NSValue *node4Content=[self setBudgetLevelTwoTypeOneNodeWithName:node4Name];
+                         [self.arrLevelTwoNode addObject:node4Content];
+                         break;
+                        }
+                 
+                    case 1:{
+                        //第二类二级结点数据类型
+                        NSValue *node4Type=[self setBudgetLevelGroupWithType:1 Category:1 Parent:m Index:n];
+                        [self.arrNodeType addObject:node4Type];
+                     
+                        //第二类二级结点数据内容
+                        NSString *node4Name=[[node4 attributeForName:@"item_name"]stringValue];
+                        NSValue *node4Content=[self setBudgetLevelTwoTypeTwoNodeWithName:node4Name];
+                        [self.arrLevelTwoNode addObject:node4Content];
+                        break;
+                        }
+                        
+                    case 2:{
+                        //第三类二级结点数据类型
+                        NSValue *node4Type=[self setBudgetLevelGroupWithType:1 Category:2 Parent:m Index:n];
+                        [self.arrNodeType addObject:node4Type];
+                        
+                        //第三类二级结点数据内容
+                        NSString *node4Name=[[node4 attributeForName:@"item_name"]stringValue];
+                        NSValue *node4Content=[self setBudgetLevelTwoTypeThreeNodeWithName:node4Name];
+                        [self.arrLevelTwoNode addObject:node4Content];
+                        break;
+                    }
+                 default:break;
+                }
+    
+            }
           
             
         }
@@ -92,6 +140,60 @@
     
     
 }
+
+//将数据结点类型结构体，转为nsvalue
+-(NSValue*)setBudgetLevelGroupWithType:(int)type  Category:(int)category Parent:(int)parent Index:(int)index
+{
+    BudgetLevelGroup budgetLevelGroup;
+    budgetLevelGroup.type=type;
+    budgetLevelGroup.category=category;
+    budgetLevelGroup.parent=parent;
+    budgetLevelGroup.index=index;
+    NSValue *budgetType=[NSValue value:&budgetLevelGroup withObjCType:@encode(BudgetLevelGroup)];
+    return budgetType;
+
+}
+
+//将一级数据结点数据，转为nsvalue，
+-(NSValue*)setBudgetLevelOneNodeWithName:(NSString*)name Budget:(NSString*)budget
+{
+    BudgetLevelOneNodeGroup budgetLevelOneNodeGroup;
+    budgetLevelOneNodeGroup.name=[name UTF8String];
+    budgetLevelOneNodeGroup.budget=[budget UTF8String];
+    NSValue *budgetNodeInLevelOne=[NSValue value:&budgetLevelOneNodeGroup withObjCType:@encode(BudgetLevelOneNodeGroup)];
+    return budgetNodeInLevelOne;
+}
+
+//将二级数据结点第一类数据，转为nsvalue，
+-(NSValue*)setBudgetLevelTwoTypeOneNodeWithName:(NSString*)name
+{
+    BudgetLevelTwoTypeOneGroup budgetLevelTwoTypeOneGroup;
+    budgetLevelTwoTypeOneGroup.name=[name UTF8String];
+    NSValue *budgetNodeInLevelTwoTypeOne=[NSValue value:&budgetLevelTwoTypeOneGroup withObjCType:@encode(BudgetLevelTwoTypeOneGroup)];
+    return budgetNodeInLevelTwoTypeOne;
+}
+
+//将二级数据结点第二类数据，转为nsvalue，
+-(NSValue*)setBudgetLevelTwoTypeTwoNodeWithName:(NSString*)name
+{
+    BudgetLevelTwoTypeTwoGroup budgetLevelTwoTypeTwoGroup;
+    budgetLevelTwoTypeTwoGroup.name=[name UTF8String];
+    NSValue *budgetNodeInLevelTwoTypeTwo=[NSValue value:&budgetLevelTwoTypeTwoGroup withObjCType:@encode(BudgetLevelTwoTypeTwoGroup)];
+    return budgetNodeInLevelTwoTypeTwo;
+}
+
+//将二级数据结点第三类数据，转为nsvalue，
+-(NSValue*)setBudgetLevelTwoTypeThreeNodeWithName:(NSString*)name
+{
+    BudgetLevelTwoTypeThreeGroup budgetLevelTwoTypeThreeGroup;
+    budgetLevelTwoTypeThreeGroup.name=[name UTF8String];
+    NSValue *budgetNodeInLevelTwoTypeThree=[NSValue value:&budgetLevelTwoTypeThreeGroup withObjCType:@encode(BudgetLevelTwoTypeThreeGroup)];
+    return budgetNodeInLevelTwoTypeThree;
+}
+
+
+//获取每个table cell的calayer的数组内容
+
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -105,11 +207,11 @@
               initWithStyle:UITableViewCellStyleValue2
               reuseIdentifier:budgetTblViewIdentifier];
         
-        CALayer *nameLayer=[self getNameLayerWithRowIndex:row];   //预算名称
-        [cell.layer addSublayer:nameLayer];
-        
-        CALayer *totalLayer=[self getTotalLayerWithRowIndex:row];  //预算金额
-        [cell.layer addSublayer:totalLayer];
+        NSMutableArray *arrCellLayers=[self getCALayersWithRowIndex:row];
+        for (CALayer *layer in arrCellLayers)
+        {
+            [cell.layer addSublayer:layer];
+        }
 
     }
     
@@ -117,8 +219,42 @@
 
 }
 
--(CALayer*)getNameLayerWithRowIndex:(NSUInteger)rowIndex
+-(NSMutableArray*)getCALayersWithRowIndex:(NSUInteger)rowIndex
 {
+    NSMutableArray *arrCALayer=[[NSMutableArray alloc]init];
+    NSValue *levelGroup=[self.arrNodeType objectAtIndex:rowIndex];
+    BudgetLevelGroup structLevelGroup;
+    [levelGroup getValue:&structLevelGroup];
+    if (structLevelGroup.type==0)
+    {
+        arrCALayer=[self getLvOneCALayersWithIndex:structLevelGroup.index];
+    }
+    else if (structLevelGroup.type==1)
+    {
+        
+        arrCALayer=[self getLvTwoCALayersWithIndex:structLevelGroup.index Category:structLevelGroup.category];
+    }
+   
+    
+    return arrCALayer;
+}
+
+//返回一级节点calayer数组
+-(NSMutableArray*)getLvOneCALayersWithIndex:(NSUInteger)index
+{
+    
+    NSMutableArray *arrLvOneCALayers=[[NSMutableArray alloc]init];
+    
+    NSValue *content=[self.arrLevelOneNode objectAtIndex:index];
+    BudgetLevelOneNodeGroup budgetLevelOneNodeGroup;
+    [content getValue:&budgetLevelOneNodeGroup];
+    
+    const char *cName=budgetLevelOneNodeGroup.name;
+    NSString *name=[NSString stringWithCString:cName encoding:NSUTF8StringEncoding];
+    
+    const char *cBudget=budgetLevelOneNodeGroup.budget;
+    NSString *budget=[NSString stringWithCString:cBudget encoding:NSUTF8StringEncoding];
+    
     CATextLayer *nameLayer=[[CATextLayer alloc]init];
     [nameLayer setFont:@"AppleGothic"];
     [nameLayer setFontSize:15];
@@ -127,41 +263,125 @@
                                    [UIScreen mainScreen].bounds.size.width*0.5f,
                                    [UIScreen mainScreen].bounds.size.height*0.5f)];
     
-    [nameLayer setString:[self.arrNode1Name objectAtIndex:rowIndex]];
+    [nameLayer setString:name];
     [nameLayer setAlignmentMode:kCAAlignmentLeft];
     [nameLayer setForegroundColor:[[UIColor blackColor] CGColor]];
     [nameLayer setContentsScale:2];
     
-    return nameLayer;
+    CATextLayer *budgetLayer=[[CATextLayer alloc]init];
+    [budgetLayer setFont:@"AppleGothic"];
+    [budgetLayer setFontSize:15];
+    [budgetLayer setFrame:CGRectMake([UIScreen mainScreen].bounds.size.width*0.6f,
+                                     [UIScreen mainScreen].bounds.size.height*0.04f,
+                                     [UIScreen mainScreen].bounds.size.width*0.4f,
+                                     [UIScreen mainScreen].bounds.size.height*0.5f)];
+    NSString *budgetStr=[NSString stringWithFormat:@"%@",budget];
+    
+    [budgetLayer setString:budgetStr];
+    [budgetLayer setAlignmentMode:kCAAlignmentLeft];
+    [budgetLayer setForegroundColor:[[UIColor blueColor] CGColor]];
+    [budgetLayer setContentsScale:2];
+    
+    [arrLvOneCALayers addObject:nameLayer];
+    [arrLvOneCALayers addObject:budgetLayer];
+    return arrLvOneCALayers;
+
+
     
 }
 
--(CALayer*)getTotalLayerWithRowIndex:(NSUInteger)rowIndex
+
+
+-(NSMutableArray*)getLvTwoCALayersWithIndex:(NSUInteger)index Category:(NSUInteger)category
 {
-    CATextLayer *nameLayer=[[CATextLayer alloc]init];
-    [nameLayer setFont:@"AppleGothic"];
-    [nameLayer setFontSize:15];
-    [nameLayer setFrame:CGRectMake([UIScreen mainScreen].bounds.size.width*0.6f,
-                                   [UIScreen mainScreen].bounds.size.height*0.04f,
-                                   [UIScreen mainScreen].bounds.size.width*0.4f,
-                                   [UIScreen mainScreen].bounds.size.height*0.5f)];
-    NSString *budgetStr=[NSString stringWithFormat:@"%@",[self.arrNode1Budget objectAtIndex:rowIndex]];
+     NSMutableArray *arrLvTwoCALayers=[[NSMutableArray alloc]init];
+     NSValue *content=[self.arrLevelTwoNode objectAtIndex:index];
     
-    [nameLayer setString:budgetStr];
-    [nameLayer setAlignmentMode:kCAAlignmentLeft];
-    [nameLayer setForegroundColor:[[UIColor blueColor] CGColor]];
-    [nameLayer setContentsScale:2];
+    switch (category)
+    {
+        case 0:{BudgetLevelTwoTypeOneGroup budgetLevelTwoTypeOneGroup;
+               [content getValue:&budgetLevelTwoTypeOneGroup];
+               const char* cName=budgetLevelTwoTypeOneGroup.name;
+               NSString *name=[NSString stringWithCString:cName encoding:NSUTF8StringEncoding];
+            
+              CATextLayer *nameLayer=[[CATextLayer alloc]init];
+              [nameLayer setFont:@"AppleGothic"];
+              [nameLayer setFontSize:15];
+              [nameLayer setFrame:CGRectMake([UIScreen mainScreen].bounds.size.width*0.02f,
+                                             [UIScreen mainScreen].bounds.size.height*0.04f,
+                                             [UIScreen mainScreen].bounds.size.width*0.5f,
+                                             [UIScreen mainScreen].bounds.size.height*0.5f)];
+            
+              [nameLayer setString:name];
+              [nameLayer setAlignmentMode:kCAAlignmentLeft];
+              [nameLayer setForegroundColor:[[UIColor blackColor] CGColor]];
+              [nameLayer setContentsScale:2];
+
+              [arrLvTwoCALayers addObject:nameLayer];
+               NSLog(@"a");
+               break;
+               }
+        case 1:{BudgetLevelTwoTypeTwoGroup budgetLevelTwoTypeTwoGroup;
+               [content getValue:&budgetLevelTwoTypeTwoGroup];
+               const char* cName=budgetLevelTwoTypeTwoGroup.name;
+               NSString *name=[NSString stringWithCString:cName encoding:NSUTF8StringEncoding];
+            
+               CATextLayer *nameLayer=[[CATextLayer alloc]init];
+               [nameLayer setFont:@"AppleGothic"];
+               [nameLayer setFontSize:15];
+               [nameLayer setFrame:CGRectMake([UIScreen mainScreen].bounds.size.width*0.02f,
+                                              [UIScreen mainScreen].bounds.size.height*0.04f,
+                                              [UIScreen mainScreen].bounds.size.width*0.5f,
+                                              [UIScreen mainScreen].bounds.size.height*0.5f)];
+            
+               [nameLayer setString:name];
+               [nameLayer setAlignmentMode:kCAAlignmentLeft];
+               [nameLayer setForegroundColor:[[UIColor blackColor] CGColor]];
+               [nameLayer setContentsScale:2];
+            
+               [arrLvTwoCALayers addObject:nameLayer];
+               NSLog(@"b");
+            
+               break;
+               }
+        case 2:{BudgetLevelTwoTypeThreeGroup budgetLevelTwoTypeThreeGroup;
+               [content getValue:&budgetLevelTwoTypeThreeGroup];
+               const char* cName=budgetLevelTwoTypeThreeGroup.name;
+               NSString *name=[NSString stringWithCString:cName encoding:NSUTF8StringEncoding];
+            
+               CATextLayer *nameLayer=[[CATextLayer alloc]init];
+               [nameLayer setFont:@"AppleGothic"];
+               [nameLayer setFontSize:15];
+               [nameLayer setFrame:CGRectMake([UIScreen mainScreen].bounds.size.width*0.02f,
+                                              [UIScreen mainScreen].bounds.size.height*0.04f,
+                                              [UIScreen mainScreen].bounds.size.width*0.5f,
+                                              [UIScreen mainScreen].bounds.size.height*0.5f)];
+            
+               [nameLayer setString:name];
+               [nameLayer setAlignmentMode:kCAAlignmentLeft];
+               [nameLayer setForegroundColor:[[UIColor blackColor] CGColor]];
+               [nameLayer setContentsScale:2];
+            
+               [arrLvTwoCALayers addObject:nameLayer];
+               NSLog(@"c");
+               break;
+               }
+        default:
+            break;
+    }
     
-    return nameLayer;
-    
+    return arrLvTwoCALayers;
 }
+
+
+
 
 
 
 
 -(NSInteger) tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.arrNode1Name.count;
+    return self.arrNodeType.count;
 }
 
 
